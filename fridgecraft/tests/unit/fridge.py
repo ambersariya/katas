d5c0cddeb11d6fd20c138ce8f46d2a5337ab9cc6
Javed, Danish <Danish.Javed@F-Secure.com>
2022-03-17 15:54:30 +0000

    refactor(tests): move local instantiation of fridge to setUp()

--- fridgecraft/tests/unit/fridge.py
+++ fridgecraft/tests/unit/fridge.py
@@ -8,6 +8,7 @@ from fridgecraft.src.item import ItemCondition
 class TestFridge(unittest.TestCase):
     def setUp(self) -> None:
         self.fridge = Fridge()
+        self.fridge.set_current_date('18/10/21')
 
     def test_not_be_able_to_add_items_when_door_is_closed(self):
         with self.assertRaises(self.fridge.DoorClosed):
@@ -25,39 +26,29 @@ class TestFridge(unittest.TestCase):
         self.assertEqual(1, len(self.fridge._items))
 
     def test_should_set_the_current_time_to_given_string(self):
-        fridge = Fridge()
-        fridge.set_current_date('18/10/21')
-        self.assertIsInstance(fridge._current_time, datetime.datetime)
-        self.assertEqual(fridge._current_time.strftime("%d/%m/%y"), '18/10/21')
+        self.assertIsInstance(self.fridge._current_time, datetime.datetime)
+        self.assertEqual(self.fridge._current_time.strftime("%d/%m/%y"), '18/10/21')
 
     def test_should_simulate_day_over_to_next_date(self):
-        fridge = Fridge()
+        self.fridge.simulate_day_over()
 
-        fridge.set_current_date('18/10/21')
-        fridge.simulate_day_over()
-
-        self.assertIsInstance(fridge._current_time, datetime.datetime)
-        self.assertEqual(fridge._current_time.strftime("%d/%m/%y"), '19/10/21')
+        self.assertIsInstance(self.fridge._current_time, datetime.datetime)
+        self.assertEqual(self.fridge._current_time.strftime("%d/%m/%y"), '19/10/21')
 
     def test_should_signal_fridge_door_closed(self):
-        fridge = Fridge()
-        fridge.signal_fridge_door_opened()
+        self.fridge.signal_fridge_door_opened()
 
-        fridge.signal_fridge_door_closed()
+        self.fridge.signal_fridge_door_closed()
 
-        self.assertEqual(fridge._state, FridgeState.DOOR_CLOSED)
+        self.assertEqual(self.fridge._state, FridgeState.DOOR_CLOSED)
 
     def test_should_not_remove_item_when_fridge_door_is_closed(self):
-        fridge = Fridge()
-        fridge.set_current_date('18/10/21')
-        with self.assertRaises(fridge.DoorClosed):
-            fridge.scan_removed_item('jam')
+        with self.assertRaises(self.fridge.DoorClosed):
+            self.fridge.scan_removed_item('jam')
 
     def test_should_not_remove_item_when_there_is_no_matching_item_with_name(self):
-        fridge = Fridge()
-        fridge.set_current_date('18/10/21')
-        fridge.signal_fridge_door_opened()
-        fridge.scan_added_item(name='bread', expiry='2021', condition=str(ItemCondition.SEALED))
+        self.fridge.signal_fridge_door_opened()
+        self.fridge.scan_added_item(name='bread', expiry='2021', condition=str(ItemCondition.SEALED))
 
-        with self.assertRaises(fridge.CannotRemoveItem):
-            fridge.scan_removed_item('jam')
+        with self.assertRaises(self.fridge.CannotRemoveItem):
+            self.fridge.scan_removed_item('jam')
