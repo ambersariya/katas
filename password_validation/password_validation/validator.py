139c69d15ca8f6b5d7cc3046ec00c23ed0b7cfc5
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-08-02 17:28:02 +0100

    feat(iteration 1): implemented all rules in password validator

--- password_validation/password_validation/validator.py
+++ password_validation/password_validation/validator.py
@@ -1,8 +1,13 @@
-from password_validation.rules.rules import HasMinPasswordLength
+from password_validation.rules.rules import HasMinPasswordLength, ContainsUnderscore, ContainsNumber, \
+    ContainsLowercaseLetter, ContainsCapitalLetter
 
 PASSWORD_LENGTH = 8
 
 
 class PasswordValidator:
     def validate(self, password: str) -> bool:
-        return HasMinPasswordLength(min_length=PASSWORD_LENGTH).check(password)
+        return HasMinPasswordLength(min_length=PASSWORD_LENGTH).check(password) \
+               and ContainsCapitalLetter().check(password) \
+               and ContainsLowercaseLetter().check(password) \
+               and ContainsNumber().check(password) \
+               and ContainsUnderscore().check(password)
