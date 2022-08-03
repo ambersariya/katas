b7cd68327a415c77f0d1f5c5316f9c55a949623c
iagardi <istvan.agardi83@gmail.com>
2022-08-03 10:17:05 +0100

    return type change for rules, returning violation class instead of boolean

--- password_validation/tests/unit/rules/test_password_length.py
+++ password_validation/tests/unit/rules/test_password_length.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
 
-from password_validation.rules.rules import HasMinPasswordLength
+from password_validation.rules.rules import HasMinPasswordLength, Violation
 
 
 class TestPasswordLengthShould(TestCase):
-    def test_check_password_matches_given_length_or_longer(self):
+    def test_should_return_violation_when_password_is_shorter_than_required(self):
         password_length = HasMinPasswordLength(min_length=6)
-        password_to_check = '123454'
-        self.assertTrue(password_length.check(password_to_check))
+        password_to_check = '12345'
+        self.assertIsInstance(password_length.check(password_to_check), Violation)
+
+    def test_should_return_nothing_when_password_is_longer_than_required(self):
+        password_length = HasMinPasswordLength(min_length=6)
+        password_to_check = '123456'
+        self.assertIsNone(password_length.check(password_to_check))
