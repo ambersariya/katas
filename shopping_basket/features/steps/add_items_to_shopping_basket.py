f8879fa568748fe2bb0a74ba4aa8ca8f2a0aefaa
iagardi <istvan.agardi83@gmail.com>
2022-08-08 16:42:02 +0100

    feat(wip): implementing scenario steps

--- shopping_basket/features/steps/add_items_to_shopping_basket.py
+++ shopping_basket/features/steps/add_items_to_shopping_basket.py
@@ -1,14 +1,26 @@
 from behave import *
 
 
+def before_scenario(context, scenario):
+    context.shopping_basket_service = ShoppingBasketService()
+    context.product_service = ProductService()
+    context.user_id = UserId()
+
+
 @given(u'I add "{quantity}" units of "{item}" to my shopping basket')
 def step_impl(context, quantity: str, item: str):
-    pass
+    item = context.product_service.get_item_by_name(name=item)
+    context.shopping_basket_service.add_item(user_id=context.user_id,
+                                             product_id=item.product_id,
+                                             quantity=int(quantity))
 
 
 @given(u'I add "{quantity}" units of "{item}"')
 def step_impl(context, quantity: str, item: str):
-    pass
+    item = context.product_service.get_item_by_name(name=item)
+    context.shopping_basket_service.add_item(user_id=context.user_id,
+                                             product_id=item.product_id,
+                                             quantity=int(quantity))
 
 
 @when(u'I check the content of my shopping basket')
