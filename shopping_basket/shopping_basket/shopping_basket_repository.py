07ae6b0df1bc006e61290a7dcb739289abe1f6db
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-08-09 14:03:48 +0100

    test(shopping basket repository): add testcase for returning no basket

--- shopping_basket/shopping_basket/shopping_basket_repository.py
+++ shopping_basket/shopping_basket/shopping_basket_repository.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Protocol, Optional
 
 from shopping_basket.shopping_basket import ShoppingBasket
@@ -5,5 +6,17 @@ from shopping_basket.user import UserId
 
 
 class ShoppingBasketRepository(Protocol):
+    @abstractmethod
     def basket_for(self, user_id: UserId) -> Optional[ShoppingBasket]:
         pass
+
+
+class InMemoryShoppingBasketRepository(ShoppingBasketRepository):
+    _baskets: dict
+
+    def __init__(self):
+        self._baskets = {}
+
+    def basket_for(self, user_id: UserId) -> Optional[ShoppingBasket]:
+        if user_id in self._baskets:
+            return self._baskets[user_id]
---
+++ shopping_basket/tests/unit_tests/test_shopping_basket_repository.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from shopping_basket.shopping_basket_repository import InMemoryShoppingBasketRepository
+from shopping_basket.user import UserId
+
+
+class InMemoryShoppingBasketRepositoryShould(TestCase):
+    def test_return_no_shopping_basket_for_user(self):
+        user_id = UserId('some-id')
+        repository = InMemoryShoppingBasketRepository()
+        result = repository.basket_for(user_id=user_id)
+        self.assertIsNone(result)
