d8fad7e276d71d3aa0fb0226eb57be8a41524752
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-08-10 09:43:07 +0100

    test(shopping basket repo): refactor testcases basket being created only when we add an item

--- shopping_basket/shopping_basket/shopping_basket_repository.py
+++ shopping_basket/shopping_basket/shopping_basket_repository.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from typing import Protocol, Optional
 
+from shopping_basket.date_provider import DateProvider
 from shopping_basket.shopping_basket import ShoppingBasket, ShoppingBasketItem
 from shopping_basket.user import UserId
 
@@ -11,26 +12,28 @@ class ShoppingBasketRepository(Protocol):
         pass
 
     @abstractmethod
-    def get_or_create(self, user_id):
-        pass
-
-    @abstractmethod
-    def add_item(self, item: ShoppingBasketItem):
+    def add_item(self, item: ShoppingBasketItem, user_id: UserId):
         pass
 
 
 class InMemoryShoppingBasketRepository(ShoppingBasketRepository):
-    def get_or_create(self, user_id):
-        raise NotImplementedError()
-
     _baskets: dict
 
-    def __init__(self):
+    def __init__(self, date_provider: DateProvider):
+        self._date_provider = date_provider
         self._baskets = {}
 
     def basket_for(self, user_id: UserId) -> Optional[ShoppingBasket]:
-        if user_id in self._baskets:
+        if self._basket_exists(user_id=user_id):
             return self._baskets[user_id]
 
-    def add_item(self, item: ShoppingBasketItem):
-        raise NotImplementedError()
+    def add_item(self, item: ShoppingBasketItem, user_id: UserId):
+        if not self._basket_exists(user_id=user_id):
+            basket = ShoppingBasket(
+                items=[item],
+                user_id=user_id,
+                created_at=self._date_provider.current_date())
+            self._baskets[user_id] = basket
+
+    def _basket_exists(self, user_id: UserId):
+        return user_id in self._baskets
--- shopping_basket/tests/unit_tests/test_shopping_basket_repository.py
+++ shopping_basket/tests/unit_tests/test_shopping_basket_repository.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 
-from shopping_basket.shopping_basket import ShoppingBasket
+from shopping_basket.date_provider import DateProvider
+from shopping_basket.product import Product, ProductId
+from shopping_basket.shopping_basket import ShoppingBasket, ShoppingBasketItem
 from shopping_basket.shopping_basket_repository import InMemoryShoppingBasketRepository
 from shopping_basket.user import UserId
 
@@ -8,16 +11,23 @@ from shopping_basket.user import UserId
 class InMemoryShoppingBasketRepositoryShould(TestCase):
     def test_return_no_shopping_basket_for_user(self):
         user_id = UserId('some-id')
-        repository = InMemoryShoppingBasketRepository()
+        date_provider = MagicMock(DateProvider)
+        repository = InMemoryShoppingBasketRepository(date_provider=date_provider)
         result = repository.basket_for(user_id=user_id)
         self.assertIsNone(result)
 
-    def test_return_shopping_basket_for_user(self):
+    def test_create_shopping_basket_only_when_we_add_an_item(self):
         user_id = UserId('some-id')
         basket_created_at = '15/06/2022'
+        quantity = 5
+        product = Product(ProductId('product-1'), name='the hobbit dvd', price=5)
+        shopping_basket_item = ShoppingBasketItem.for_product(product=product, quantity=quantity)
+        date_provider = MagicMock(DateProvider)
+        date_provider.current_date.return_value = basket_created_at
 
-        repository = InMemoryShoppingBasketRepository()
-        repository.add(basket=ShoppingBasket(user_id=user_id, created_at=basket_created_at, items=None))
+        repository = InMemoryShoppingBasketRepository(date_provider=date_provider)
+        repository.add_item(item=shopping_basket_item, user_id=user_id)
         result = repository.basket_for(user_id=user_id)
 
         self.assertIsInstance(result, ShoppingBasket)
+        self.assertEqual(1, len(result.items))
