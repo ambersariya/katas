f109657492f9e773d8c3a8dbc0502b0504c89901
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-08-10 09:45:52 +0100

    refactor: cleanup testcase for shopping basket repo, introduce constants.

--- shopping_basket/tests/unit_tests/test_shopping_basket_repository.py
+++ shopping_basket/tests/unit_tests/test_shopping_basket_repository.py
@@ -7,27 +7,25 @@ from shopping_basket.shopping_basket import ShoppingBasket, ShoppingBasketItem
 from shopping_basket.shopping_basket_repository import InMemoryShoppingBasketRepository
 from shopping_basket.user import UserId
 
+USER_ID = UserId('some-id')
+PRODUCT = Product(ProductId('product-1'), name='the hobbit dvd', price=5)
+BASKET_ITEM = ShoppingBasketItem.for_product(product=PRODUCT, quantity=5)
+
 
 class InMemoryShoppingBasketRepositoryShould(TestCase):
+    def setUp(self) -> None:
+        self.date_provider = MagicMock(DateProvider)
+        self.repository = InMemoryShoppingBasketRepository(date_provider=self.date_provider)
+
     def test_return_no_shopping_basket_for_user(self):
-        user_id = UserId('some-id')
-        date_provider = MagicMock(DateProvider)
-        repository = InMemoryShoppingBasketRepository(date_provider=date_provider)
-        result = repository.basket_for(user_id=user_id)
+        result = self.repository.basket_for(user_id=USER_ID)
         self.assertIsNone(result)
 
     def test_create_shopping_basket_only_when_we_add_an_item(self):
-        user_id = UserId('some-id')
-        basket_created_at = '15/06/2022'
-        quantity = 5
-        product = Product(ProductId('product-1'), name='the hobbit dvd', price=5)
-        shopping_basket_item = ShoppingBasketItem.for_product(product=product, quantity=quantity)
-        date_provider = MagicMock(DateProvider)
-        date_provider.current_date.return_value = basket_created_at
+        self.date_provider.current_date.return_value = '15/06/2022'
+        self.repository.add_item(item=BASKET_ITEM, user_id=USER_ID)
 
-        repository = InMemoryShoppingBasketRepository(date_provider=date_provider)
-        repository.add_item(item=shopping_basket_item, user_id=user_id)
-        result = repository.basket_for(user_id=user_id)
+        result = self.repository.basket_for(user_id=USER_ID)
 
         self.assertIsInstance(result, ShoppingBasket)
         self.assertEqual(1, len(result.items))
