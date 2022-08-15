e392392c3d98e94bda9ada4fc0d616167a9b2d07
iagardi <istvan.agardi83@gmail.com>
2022-08-15 11:59:42 +0100

    test(acceptance test): acceptance test for scenario 4 is complete, fails for the right reason refactor : acceptance test updated for stock management

--- shopping_basket/tests/acceptance_tests/test_should_apply_discount.py
+++ shopping_basket/tests/acceptance_tests/test_should_apply_discount.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import MagicMock
 
 from shopping_basket.date_provider import DateProvider
-from shopping_basket.errors import InsufficientStockError
 from shopping_basket.product import Product, ProductId
 from shopping_basket.product_repository import InMemoryProductRepository
 from shopping_basket.product_service import ProductService
@@ -14,37 +13,39 @@ from shopping_basket.stock.stock_repository import InMemoryStockRepository
 from shopping_basket.user import UserId
 from shopping_basket.utilities import ItemLogger
 
-PRODUCT_ID = ProductId('10001')
 
-
-class ManageStockAvailabilityShould(TestCase):
+class ApplyDiscountShould(TestCase):
 
     def setUp(self):
         date_provider = MagicMock(DateProvider)
         date_provider.current_date.return_value = "14/6/2022"
         self.shopping_basket_repository = InMemoryShoppingBasketRepository(date_provider=date_provider)
-        self.product_repository = InMemoryProductRepository()
         self.stock_repository = InMemoryStockRepository()
         self.stock_management_service = StockManagementService(self.stock_repository)
-        self.product_service = ProductService(self.product_repository, self.stock_management_service)
+        self.product_repository = InMemoryProductRepository()
+        self.product_service = ProductService(product_repository=self.product_repository,
+                                              stock_management_service=self.stock_management_service)
         self.item_logger = ItemLogger()
         self.shopping_basket_service = ShoppingBasketService(product_service=self.product_service,
                                                              shopping_basket_repository=self.shopping_basket_repository,
                                                              item_logger=self.item_logger)
         self.user_id = UserId('user-01')
         self._fill_products()
-        self.stock_management_service.save_stock(stock=Stock(product_id=PRODUCT_ID, available=3, reserved=0))
 
-    def test_raise_error_when_not_enough_product_in_stock(self):
-        with self.assertRaises(InsufficientStockError):
-            self._add_item(user_id=self.user_id, product_id=PRODUCT_ID, quantity=5)
+    def test_return_contents_of_the_basket(self):
+        self._add_item(self.user_id, ProductId("10002"), 4)
+        self._add_item(self.user_id, ProductId("20110"), 5)
 
-    def test_add_item_to_basket_when_stock_is_sufficient(self):
-        self._add_item(user_id=self.user_id, product_id=PRODUCT_ID, quantity=2)
-        actual_stock = self.stock_repository.find_by_id(PRODUCT_ID)
-        expected_stock = Stock(product_id=PRODUCT_ID, available=1, reserved=2)
+        basket = self.shopping_basket_service.basket_for(self.user_id)
 
-        self.assertEqual(expected_stock, actual_stock)
+        basket_printout = \
+            "Creation date 14/6/2022\n" \
+            "4 x The Hobbit // Books // 4 x 5.00 = £20.00\n" \
+            "5 x Breaking Bad // Video // 5 x 7.00 = £35.00\n" \
+            "Discount applied: 20% \n" \
+            "Total: £44.00"
+
+        assert str(basket) == basket_printout
 
     def _add_item(self, user_id: UserId, product_id: ProductId, quantity: int):
         self.shopping_basket_service.add_item(user_id=user_id,
