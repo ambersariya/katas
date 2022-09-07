22ae68e618486b74dc2845d044cb80334861493e
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-07 09:52:41 +0100

    refactor: convert to pytest

--- shopping_basket/tests/unit_tests/conftest.py
+++ shopping_basket/tests/unit_tests/conftest.py
@@ -2,6 +2,9 @@ from unittest.mock import MagicMock
 
 from _pytest.fixtures import fixture
 
+from shopping_basket.basket.infrastructure.in_memory_shopping_basket_repository import \
+    InMemoryShoppingBasketRepository
+from shopping_basket.basket.shopping_basket_repository import ShoppingBasketRepository
 from shopping_basket.order.order_repository import OrderRepository
 from shopping_basket.payment.infrastructure.payment_gateway import PaymentGateway
 from shopping_basket.payment.infrastructure.payment_provider import PaymentProvider
@@ -42,3 +45,13 @@ def payment_gateway(mocked_payment_provider, mocked_order_repository):
         payment_provider=mocked_payment_provider,
         order_repository=mocked_order_repository,
     )
+
+
+@fixture()
+def in_memory_shopping_basket_repo(date_provider):
+    return InMemoryShoppingBasketRepository(date_provider=date_provider)
+
+
+@fixture()
+def mock_shopping_basket_repo():
+    MagicMock(ShoppingBasketRepository)
