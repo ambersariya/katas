2ae4bcda1652697f5184db804de6b95235ac9ab8
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-09-27 10:59:15 +0100

    test: add testcase for formatting month of birth based on gender

--- driving-licence/driving_licence/generator.py
+++ driving-licence/driving_licence/generator.py
@@ -9,3 +9,6 @@ class DrivingLicenceGenerator:
         return datetime.datetime \
             .strptime(dob, '%m-%b-%Y') \
             .strftime('%y')[0]
+
+    def format_month_of_birth(self, dob: str, gender: str) -> str:
+        pass
--- driving-licence/tests/test_driving_licence_generator.py
+++ driving-licence/tests/test_driving_licence_generator.py
@@ -23,3 +23,14 @@ def test_should_retrieve_decade(data, expected_output):
     driving_licence_generator = DrivingLicenceGenerator()
     result = driving_licence_generator.format_year(dob=data)
     assert result == expected_output
+
+
+@pytest.mark.parametrize('data, expected_output', [
+    (["01-Jan-1980", "M"], '01'),
+    (["01-Feb-1995", "F"], '51'),
+    (["01-Dec-2000", "F"], '62')
+])
+def test_should_format_month_of_birth_based_on_gender(data, expected_output):
+    driving_licence_generator = DrivingLicenceGenerator()
+    result = driving_licence_generator.format_month_of_birth(dob=data[0], gender=data[1])
+    assert result == expected_output
