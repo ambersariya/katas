0913d294986a4f78924472a0fd5b0b51f2518c18
iagardi <istvan.agardi83@gmail.com>
2022-11-28 11:29:24 +0000

    test: red test for answer queries test case

--- merchants-guide-to-galaxy/merchants_guide_to_galaxy/answer_queries_use_case.py
+++ merchants-guide-to-galaxy/merchants_guide_to_galaxy/answer_queries_use_case.py
@@ -1,5 +1,21 @@
+from dataclasses import dataclass
+
 from merchants_guide_to_galaxy.currency_repo import SymbolRepo
+from merchants_guide_to_galaxy.roman_numerals_converter import roman_to_arabic
 
 
-def answer_queries_use_case(raw_data: str, symbol_repo: SymbolRepo) -> list:
-    raise NotImplementedError()
+@dataclass(init=True, frozen=True)
+class Answer:
+    query: str
+    outcome: str
+
+
+def answer_queries_use_case(raw_data: str, symbol_repo: SymbolRepo) -> list[Answer]:
+    intergalactic_gibberish = raw_data.replace("how much is ", "").replace(" ?", "")
+    roman_symbol = list(map(
+        lambda curr: symbol_repo.symbol_value(symbol=curr).value,
+        intergalactic_gibberish.split(" ")
+    ))
+    roman_symbol = ''.join(roman_symbol)
+    arabic_value = roman_to_arabic(roman_symbol)
+    return [Answer(query=raw_data, outcome=f"{intergalactic_gibberish} is {arabic_value}")]
--- merchants-guide-to-galaxy/tests/unit/test_answer_queries_use_case.py
+++ merchants-guide-to-galaxy/tests/unit/test_answer_queries_use_case.py
@@ -1,13 +1,22 @@
 import pytest
 
 from merchants_guide_to_galaxy.answer_queries_use_case import answer_queries_use_case
+from merchants_guide_to_galaxy.currency_repo import CurrencyValue, Currency
 
 
 @pytest.mark.parametrize('input_data, answer, symbol_values', [
-    ("how much is pish tegj glob glob ?", "pish tegj glob glob is 42", ["X", "L", "I", "I"])
-])
+    ("how much is pish tegj glob glob ?", "pish tegj glob glob is 42",
+     [Currency(value=CurrencyValue.X.value, name="pish"),
+      Currency(value=CurrencyValue.L.value, name="tegj"),
+      Currency(value=CurrencyValue.I.value, name="glob"),
+      Currency(value=CurrencyValue.I.value, name="glob")
+      ])])
 def test_answer_queries_use_case(input_data, answer, symbol_values, mocked_symbol_repo):
-    answers = answer_queries_use_case(raw_data=input_data, symbol_repo=mocked_symbol_repo)
     mocked_symbol_repo.symbol_value.side_effect = symbol_values
+    answers = answer_queries_use_case(raw_data=input_data, symbol_repo=mocked_symbol_repo)
 
     assert len(answers) == 1
+
+
+x = lambda a : a + 10
+print(x(5))
