9c52e49dd84c0b455d4f352fb176c0291677dee9
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-02-09 11:00:41 +0000

    refactor: remove un-needed method

--- kata-airways/src/flight_scheduler.py
+++ kata-airways/src/flight_scheduler.py
@@ -1,11 +1,9 @@
-import random
 from typing import List
 
 from src.core.route_map import RouteMap
 from src.core.value_objects import PilotName
-from src.flight import Flight, FlightPairing
+from src.flight import Flight
 from src.flight_pairing_generator import PilotService
-
 from src.schedule import Schedule
 
 
@@ -26,22 +24,3 @@ class FlightScheduler:
         route = self.__route_map.get_route(origin=unscheduled_flight.origin, destination=unscheduled_flight.destination)
         flight_pairing = self.__flight_pairing_generator.generate_pairing(pilots, route)
         return Flight(route, unscheduled_flight.date, flight_pairing)
-
-    @staticmethod
-    def __create_flight_pairing(pilots: list[PilotName]):
-        valid_num_for_pairing = len(pilots) % 2
-        if valid_num_for_pairing > 0:
-            raise InsufficientPilotsForPairing()
-        _pilots = pilots.copy()
-        random.shuffle(_pilots)
-
-        def pick_pilots(_pilots):
-            index = random.randint(0, len(_pilots) - 1)
-            p = _pilots[index]
-            del _pilots[index]
-            return p
-
-        captain = pick_pilots(_pilots)
-        co_pilot = pick_pilots(_pilots)
-
-        return FlightPairing(captain=captain, co_pilot=co_pilot)
