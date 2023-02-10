0ef27c7a90168c4bfc33a89c6b38ff42e04480ce
Danish Javed <1080238+ambersariya@users.noreply.github.com>
2023-02-10 08:34:01 +0000

    refactor: use fluent assertion

--- kata-airways/tests/unit/core/test_route_map.py
+++ kata-airways/tests/unit/core/test_route_map.py
@@ -1,5 +1,5 @@
 import pytest
-
+from assertpy import assert_that
 from src.core.errors import UnknownDestination
 from src.core.route_map import RouteMap
 from src.core.value_objects import Route, Airport
@@ -34,7 +34,7 @@ def test_should_return_the_route_for_given_origin_and_destination():
     route_map = RouteMap(allowed_routes=ALLOWED_ROUTES)
     result = route_map.get_route(origin=AIRPORT_LHR, destination=AIRPORT_LAX)
 
-    assert type(result) == Route
-    assert result.origin == AIRPORT_LHR
-    assert result.destination == AIRPORT_LAX
-    assert result.duration == 11
+    assert_that(result).is_instance_of(Route)
+    assert_that(result.origin).is_equal_to(AIRPORT_LHR)
+    assert_that(result.destination).is_equal_to(AIRPORT_LAX)
+    assert_that(result.duration).is_equal_to(11)
