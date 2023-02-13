c5cd61caa1452b6508188a5ecfdd89a641d38b12
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-02-13 15:44:41 +0000

    refactor: use parameterised testing

--- fizzbuzz/src/fizzbuzz.test.ts
+++ fizzbuzz/src/fizzbuzz.test.ts
@@ -4,22 +4,14 @@ import { describe, expect, test } from "@jest/globals";
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz should", () => {
-  test("return '1' when given 1", () => {
-    expect(fizzbuzz(1)).toBe("1");
-  });
-  test("return '2' when given 2", () => {
-    expect(fizzbuzz(2)).toBe("2");
-  });
-  test("return 'Fizz' when given 3", () => {
-    expect(fizzbuzz(3)).toBe("Fizz");
-  });
-  test("return 'Buzz' when given 5", () => {
-    expect(fizzbuzz(5)).toBe("Buzz");
-  });
-  test("return 'FizzBuzz' when given 15", () => {
-    expect(fizzbuzz(15)).toBe("FizzBuzz");
-  });
-  test("return 'FizzBuzz' when given 75", () => {
-    expect(fizzbuzz(75)).toBe("FizzBuzz");
+  test.each([
+    [1, "1"],
+    [2, "2"],
+    [3, "Fizz"],
+    [5, "Buzz"],
+    [15, "FizzBuzz"],
+    [75, "FizzBuzz"],
+  ])("return %p given %p", (number: Number, result: String) => {
+    expect(fizzbuzz(number)).toEqual(result);
   });
 });
