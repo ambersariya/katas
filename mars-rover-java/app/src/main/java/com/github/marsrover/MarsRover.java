12abce8e34d424d122feb6e195e37c52cc2f10b6
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-05-16 10:26:26 +0100

    refactor: extract Direction (turning) out of Mars Rover class

--- mars-rover-java/app/src/main/java/com/github/marsrover/MarsRover.java
+++ mars-rover-java/app/src/main/java/com/github/marsrover/MarsRover.java
@@ -1,56 +1,24 @@
 package com.github.marsrover;
 
 public class MarsRover {
-    private String direction;
+    private Direction currentDirection;
 
     public MarsRover() {
-        direction = "N";
+        currentDirection = Direction.NORTH;
     }
 
     public String execute(String command) {
         for (char cmd : command.toCharArray()) {
             String singleCommand = String.valueOf(cmd);
             if (singleCommand.equals("R")) {
-                direction = turnRight();
+                currentDirection = currentDirection.turnRight();
             }
 
             if (singleCommand.equals("L")) {
-                direction = turnLeft();
+                currentDirection = currentDirection.turnLeft();
             }
         }
 
-        return "0:0:" + direction;
-    }
-
-    private String turnLeft() {
-        if (direction.equals("N")) {
-            return "W";
-        }
-
-        if (direction.equals("W")) {
-            return "S";
-        }
-
-        if (direction.equals("S")) {
-            return "E";
-        }
-
-        return "N";
-    }
-
-    private String turnRight() {
-        if (direction.equals("N")) {
-            return "E";
-        }
-
-        if (direction.equals("E")) {
-            return "S";
-        }
-
-        if (direction.equals("S")) {
-            return "W";
-        }
-
-        return "N";
+        return "0:0:" + currentDirection.value();
     }
 }
