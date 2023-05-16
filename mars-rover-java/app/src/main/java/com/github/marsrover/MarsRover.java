36fdfa3a21fb656f81151e23907f454353260d0f
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-05-16 11:55:18 +0100

    feat: add moving with direction

--- mars-rover-java/app/src/main/java/com/github/marsrover/MarsRover.java
+++ mars-rover-java/app/src/main/java/com/github/marsrover/MarsRover.java
@@ -1,7 +1,11 @@
 package com.github.marsrover;
 
 public class MarsRover {
+    private static final int GRID_SIZE_X = 10;
+    private static final int GRID_SIZE_Y = 10;
     private Direction currentDirection;
+    private int Y;
+    private int X;
 
     public MarsRover() {
         currentDirection = Direction.NORTH;
@@ -17,8 +21,38 @@ public class MarsRover {
             if (singleCommand.equals("L")) {
                 currentDirection = currentDirection.turnLeft();
             }
+
+            if (singleCommand.equals("M")) {
+                if (currentDirection.equals(Direction.NORTH)) {
+                    this.Y += 1;
+                }
+
+                if (currentDirection.equals(Direction.WEST)) {
+                    this.X -= 1;
+                }
+
+                if (currentDirection.equals(Direction.SOUTH)) {
+                    this.Y -= 1;
+                }
+
+                if (currentDirection.equals(Direction.EAST)) {
+                    this.X -= 1;
+                }
+
+                if (this.Y > GRID_SIZE_Y) {
+                    this.Y = 0;
+                }
+
+                if (this.X > GRID_SIZE_X) {
+                    this.X = 0;
+                }
+            }
         }
 
-        return "0:0:" + currentDirection.value();
+        return facing();
+    }
+
+    private String facing() {
+        return String.format("%d:%d:%s", Math.abs(X), Math.abs(Y), currentDirection.value());
     }
 }
