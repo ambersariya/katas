05b08e9288e86b5937760c18e3a76502f89a26fc
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-05-16 11:55:22 +0100

    feat: add moving with direction

--- mars-rover-java/app/src/test/java/com/github/marsrover/MarsRoverShould.java
+++ mars-rover-java/app/src/test/java/com/github/marsrover/MarsRoverShould.java
@@ -1,11 +1,19 @@
 package com.github.marsrover;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverShould {
+    MarsRover marsRover;
+
+    @BeforeEach
+    public void setUp() {
+        marsRover = new MarsRover();
+    }
+
     @ParameterizedTest
     @CsvSource({
             "\"\", 0:0:N",
@@ -15,7 +23,6 @@ class MarsRoverShould {
             "RRRR,0:0:N",
     })
     void execute_commands_to_turn_right(String input, String expected_output) {
-        MarsRover marsRover = new MarsRover();
         assertEquals(marsRover.execute(input), expected_output);
     }
 
@@ -28,7 +35,23 @@ class MarsRoverShould {
             "LLLL,0:0:N",
     })
     void execute_commands_to_turn_left(String input, String expected_output) {
-        MarsRover marsRover = new MarsRover();
         assertEquals(marsRover.execute(input), expected_output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "M,0:1:N",
+            "MM,0:2:N",
+            "MMM,0:3:N",
+            "MMMMMMMMMM,0:10:N",
+            "MMMMMMMMMMM,0:0:N",
+            "LM,1:0:W",
+            "LLM,0:1:S",
+            "LLLM,1:0:E",
+            "RMR,1:0:S",
+            "MMRMMLM,2:3:N",
+    })
+    void execute_commands_to_move_rover(String input, String expected_output) {
+        assertEquals(expected_output, marsRover.execute(input));
     }
 }
