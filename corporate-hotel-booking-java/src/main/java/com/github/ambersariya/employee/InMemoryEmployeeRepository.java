7ac3613b79b72eae25f728311503a6202e02cad1
ambersariya <1080238+ambersariya@users.noreply.github.com>
2023-06-08 15:36:19 +0100

    refactor: make employees hashmap final

--- corporate-hotel-booking-java/src/main/java/com/github/ambersariya/employee/InMemoryEmployeeRepository.java
+++ corporate-hotel-booking-java/src/main/java/com/github/ambersariya/employee/InMemoryEmployeeRepository.java
@@ -3,7 +3,7 @@ package com.github.ambersariya.employee;
 import java.util.HashMap;
 
 public class InMemoryEmployeeRepository implements EmployeeRepository {
-    private HashMap<Integer, Employee> employees = new HashMap<>();
+    private final HashMap<Integer, Employee> employees = new HashMap<>();
 
     @Override
     public void addEmployee(Employee employee) {
